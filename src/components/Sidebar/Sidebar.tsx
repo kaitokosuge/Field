cd478c5c7a67ad07034807a8ed4059c127524060
kaitokosuge <kaitokosuge.mi@gmail.com>
2023-10-30 15:59:25 +0900

    [add]sidebar追加

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebarLeft'>
+        left
+      </div>
+      <div className='sidebarRight'>
+        right
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
